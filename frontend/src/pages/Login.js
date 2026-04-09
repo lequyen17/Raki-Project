664fc3d71c1e923abf9c696c2c14e5edf5d76861
Hà Lệ Quyên <halequyen1725@gmail.com>
2026-04-09 23:33:10 +0700

    add desk

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
             if (res.data.user.is_staff) {
                 window.location.href = 'http://127.0.0.1:8000/admin/';
             } else {
-                navigate('/');
+                navigate('/decks');
             }
         } catch (err) {
             if (err.response && (err.response.status === 400 || err.response.status === 401)) {
