c8b8913f82abcd62adf0e06ba96e02ca07e1347b
lequyen17 <halequyen1725@gmail.com>
2026-05-28 11:45:09 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Decks from "./pages/Decks/Decks";
 import Cards from "./pages/Cards/Cards";
 import AddCard from "./pages/AddCard/AddCard";
 import Study from "./pages/Study/Study";
+import Community from "./pages/Community/Community";
 import CardDetail from "./pages/CardDetail/CardDetail";
 import Header from "./components/Common/Header/Header";
 import Footer from "./components/Common/Footer/Footer";
@@ -33,6 +34,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/settings" element={<Setting />} />
           <Route path="/cards/:cardId" element={<CardDetail />} />
+          <Route path="/community" element={<Community />} />
         </Routes>
         <Footer />
       </AuthProvider>
---
+++ frontend/src/pages/Community/Community.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./Community.css";
+
+function Community() {
+  return (
+    <div className="community-container">
+      <h1>Community Decks</h1>
+    </div>
+  );
+}
+
+export default Community;
