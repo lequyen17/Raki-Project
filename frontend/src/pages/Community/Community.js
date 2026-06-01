23dd9171b15e3c5b008d1a0b65f7d8ab894883ae
lequyen17 <halequyen1725@gmail.com>
2026-06-01 10:11:03 +0700

    update

--- frontend/src/pages/Community/Community.js
+++ frontend/src/pages/Community/Community.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Community.css";
 
 function Community() {
-  return (
-    <div className="community-container">
-      <h1>Community Decks</h1>
-    </div>
-  );
+  return <div className="community-container"></div>;
 }
 
 export default Community;
--- raki/deck/models.py
+++ raki/deck/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class Deck(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subdecks')
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="subdecks"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     is_public = models.BooleanField(default=False)
 
@@ -16,27 +18,18 @@ class Deck(models.Model):
 class UserDeck(models.Model):
     # Định nghĩa các vai trò trong một bộ thẻ
     ROLE_CHOICES = [
-        ('owner', 'Owner'),
-        ('editor', 'Editor'),
-        ('viewer', 'Viewer'),
+        ("owner", "Owner"),
+        ("viewer", "Viewer"),
     ]
-    
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_decks')
-    
-    deck = models.ForeignKey(
-        Deck, 
-        on_delete=models.CASCADE,
-        related_name='deck_users'
-    )
-    role = models.CharField(
-        max_length=20, 
-        choices=ROLE_CHOICES, 
-        default='viewer'
-    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_decks")
+
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name="deck_users")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="viewer")
 
     class Meta:
         # Đảm bảo một user không bị trùng lặp vai trò trong cùng một deck
-        unique_together = ('user', 'deck')
+        unique_together = ("user", "deck")
 
     def __str__(self):
         return f"{self.user.username} - {self.deck.name} ({self.role})"
