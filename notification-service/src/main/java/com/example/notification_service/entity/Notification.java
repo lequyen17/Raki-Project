3c357b2b99a90352dd046167b201380ffe67eb51
lequyen17 <halequyen1725@gmail.com>
2026-07-30 09:39:22 +0700

    update notification service

---
+++ notification-service/src/main/java/com/example/notification_service/entity/Notification.java
@@ -0,0 +1,36 @@
+package com.example.notification_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "type", length = 50)
+    private String type;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead = false; // Mặc định là chưa đọc
+
+    @Column(name = "action_url")
+    private String actionUrl;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
